2348f3663ac1da77196b32a9f596ad728fbe3137
Akash raj(CMD) <akashraj9828@gmail.com>
2020-05-08 17:19:31 +0530

    key change again

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ rm -rf build/
 npm install
 npm run build
 openssl aes-256-cbc -a -d -in key.pem.enc -out key.pem -k $ENC_KEY
-sudo chmod 600 key.pem
-sudo ssh -o StrictHostKeyChecking=no -i key.pem -p 65002 $USER@$SERVER "rm -rf domains/akashraj.tech/public_html/corona/interactive/*"
-sudo scp -o  StrictHostKeyChecking=no -P 65002 -i key.pem  -r build/* $USER@$SERVER:domains/akashraj.tech/public_html/corona/interactive
+chmod 600 key.pem
+ssh -o StrictHostKeyChecking=no -i key.pem -p 65002 $USER@$SERVER "rm -rf domains/akashraj.tech/public_html/corona/interactive/*"
+scp -o  StrictHostKeyChecking=no -P 65002 -i key.pem  -r build/* $USER@$SERVER:domains/akashraj.tech/public_html/corona/interactive
